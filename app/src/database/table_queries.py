952681d68aa039e2a637cdd59b9f768106064beb
aaguirre <agrr.68@gmail.com>
2024-01-18 20:17:35 -0600

    aaguirre - revised fields

--- app/src/database/table_queries.py
+++ app/src/database/table_queries.py
@@ -1,14 +1,14 @@
 CREATE_COMPLETED_WORK_ORDERS_TABLE = """
 CREATE TABLE IF NOT EXISTS public.completed_work_orders(
-    week INT,
-    work_orders VARCHAR(255) PRIMARY KEY,
+    start_week INT,
+	finish_week INT,
+    work_order VARCHAR(255) PRIMARY KEY,
+    priority VARCHAR(255),
     status VARCHAR(255),
     engineer VARCHAR(255),
     technician VARCHAR(255),
-    priority VARCHAR(255),
-    modification_date TIMESTAMP,
-    wip_date TIMESTAMP,
-    finished_date TIMESTAMP,
-    finished_week INT
+    modified_by VARCHAR(255),
+    start_date TIMESTAMP,
+    finish_date TIMESTAMP
 );
 """
